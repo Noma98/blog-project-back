a9589735cd07542730374c7c0acc526e272b3e54
Noma <wiostz98kr@gmail.com>
2021-07-20 14:55:24 +0900

    Password hashing
    ➕ bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
 const userSchema = new mongoose.Schema({
     email: { type: String, unique: true, required: true },
     pwd: { type: String, required: true },
@@ -8,5 +10,13 @@ const userSchema = new mongoose.Schema({
         blogName: { type: String, default: "블로그 이름을 설정하세요.", maxLength: 15 },
     }
 });
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("pwd")) {
+        next();
+    }
+    this.pwd = await bcrypt.hash(this.pwd, 10);
+    next();
+})
+
 const User = mongoose.model("User", userSchema);
 export default User;
