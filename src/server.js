3865cc3a707501de2fd0f91ffead838084dfb3da
Noma <wiostz98kr@gmail.com>
2021-08-13 22:07:43 +0900

    Change cors settings

--- src/server.js
+++ src/server.js
@@ -6,30 +6,13 @@ import cookieParser from 'cookie-parser';
 import folderRouter from './routes/api/folderRouter.js';
 import postRouter from './routes/api/postRouter.js';
 const app = express();
-// const whitelist = [
-//     'http://localhost:3000',
-//     'https://nomalog.netlify.app',
-//     'http://nomalog.netlify.app',
-// ];
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error("🔥Not allowed by CORS"))
-//         }
-//     }
-// }
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3000', 'https://nomalog.netlify.app'], credentials: true }));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.json({ "hi": "hello world!" });
-})
 app.use("/api/users", userRouter);
 app.use("/api/folders", folderRouter);
 app.use("/api/posts", postRouter);
